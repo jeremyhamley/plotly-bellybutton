c21965ba4af9b373162229b81df23fa27384b2e0
Jeremy Hamley <jeremyhamley@gmail.com>
2020-09-02 16:17:46 -0400

    save

--- static/js/app.js
+++ static/js/app.js
@@ -9,10 +9,44 @@ function unpack(rows, index) {
 
 d3.json("samples.json").then(function(data) {
     var ids = unpack(data.metadata, "id");
-    console.log(data)
+
+
+    console.log(data);
     console.log(ids);
+
 });
 
+// Load the ids into the dropdown menu
+d3.select(".well")
+    .selectAll("select")
+    .data(ids)
+    .enter()
+    .append("option")
+    .html(function(d) {
+      return `<option value ="${d}">${d}</option>`;
+    });
 
 
+
+
+
+
+
+// Bar Chart
+var trace1 = {
+    x: data.map(row => row.pair),
+    y: data.map(row => row.greekSearchResults),
+    text: data.map(row => row.greekName),
+    name: "Greek",
+    type: "bar"
+};
+      
+var data = [trace1];
+      
+var layout = {
+    title: "OTU Sample Count",
+    barmode: "group"
+};
+      
+Plotly.newPlot("bar", data, layout);
 
