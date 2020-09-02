b6b9211f8c5ff59882b02a886f999742f85476ab
Jeremy Hamley <jeremyhamley@gmail.com>
2020-09-02 17:33:39 -0400

    Load ids into dropdown

--- static/js/app.js
+++ static/js/app.js
@@ -9,15 +9,7 @@ function unpack(rows, index) {
 
 d3.json("samples.json").then(function(data) {
     var ids = unpack(data.metadata, "id");
-
-
-    console.log(data);
-    console.log(ids);
-
-});
-
-// Load the ids into the dropdown menu
-d3.select(".well")
+    d3.select("#selDataset")
     .selectAll("select")
     .data(ids)
     .enter()
@@ -26,27 +18,36 @@ d3.select(".well")
       return `<option value ="${d}">${d}</option>`;
     });
 
+    console.log(data);
+    console.log(ids);
+});
 
 
 
 
 
 
-// Bar Chart
-var trace1 = {
-    x: data.map(row => row.pair),
-    y: data.map(row => row.greekSearchResults),
-    text: data.map(row => row.greekName),
-    name: "Greek",
-    type: "bar"
-};
+
+// sample_otu_ids = unpack(data.samples, "otu_ids")
+// console.log(sample_otu_ids)
+
+
+
+// // Bar Chart
+// var trace1 = {
+//     x: ,
+//     y: ,
+//     text:,
+//     name: ,
+//     type: "bar"
+// };
       
-var data = [trace1];
+// var data = [trace1];
       
-var layout = {
-    title: "OTU Sample Count",
-    barmode: "group"
-};
+// var layout = {
+//     title: "OTU Sample Count",
+//     barmode: "group"
+// };
       
-Plotly.newPlot("bar", data, layout);
+// Plotly.newPlot("bar", data, layout);
 
